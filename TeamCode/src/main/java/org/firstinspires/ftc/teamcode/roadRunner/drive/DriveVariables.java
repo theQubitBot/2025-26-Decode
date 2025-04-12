0b783b93e25d231d38f69e10e3629f08b086c75f
theQubitBot <88004480+theQubitBot@users.noreply.github.com>
2025-04-12 11:49:19 -0500

    Removed roadrunner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadRunner/drive/DriveVariables.java
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode.roadRunner.drive;
-
-/*
- * Variables shared between multiple drive types.
- */
-public class DriveVariables {
-    public static final boolean use2WheelTrackingLocalizer = true;
-    public static final boolean use3WheelTrackingLocalizer = false;
-}
